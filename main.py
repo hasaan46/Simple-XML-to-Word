f1f0002a8e00a5a2c569ca24748f2f7d20a2f2a7
hasan <113835232+hasaan46@users.noreply.github.com>
2023-09-13 19:13:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,40 @@
-import json
+import xml.etree.ElementTree as ET
 from docx import Document
 from tkinter import Tk, Label, Button, filedialog, messagebox
-from tkinter import CENTER
 
+# XML to Word Project by Hasan
 
-# XML Project by Hasan
-
-def select_json_file():
+def select_xml_file():
     root = Tk()
     root.withdraw()
-    json_file = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
-    if json_file:
-        generate_word(json_file)
+    xml_file = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
+    if xml_file:
+        generate_word(xml_file)
         messagebox.showinfo("Information", "Word-Datei wurde erstellt.")
 
-def generate_word(json_file, indent=0):
-    with open(json_file) as file:
-        json_data = json.load(file)
+def generate_word(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
 
-        doc = Document()
-        for key, value in json_data.items():
-            doc.add_paragraph(f"{get_indent(indent)}{key}: {format_value(value, indent)}")
+    doc = Document()
+    process_element(root, doc)
 
-        word_file = filedialog.asksaveasfilename(defaultextension=".docx", filetypes=[("Word files", "*.docx")])
-        if word_file:
-            doc.save(word_file)
-            messagebox.showinfo("Information", f"Word-Datei wurde als {word_file} gespeichert.")
+    word_file = filedialog.asksaveasfilename(defaultextension=".docx", filetypes=[("Word files", "*.docx")])
+    if word_file:
+        doc.save(word_file)
+        messagebox.showinfo("Information", f"Word-Datei wurde als {word_file} gespeichert.")
 
-def format_value(value, indent):
-    if isinstance(value, dict):
-        result = ""
-        for k, v in value.items():
-            result += f"\n{get_indent(indent+1)}{k}: {format_value(v, indent+1)}"
-        return result
-    elif isinstance(value, list):
-        result = ""
-        for item in value:
-            result += f"\n{get_indent(indent+1)}- {format_value(item, indent+1)}"
-        return result
-    else:
-        return str(value)
+def process_element(element, doc, indent=0):
+    doc.add_paragraph(f"{get_indent(indent)}{element.tag}: {element.text.strip()}")
+    for child in element:
+        process_element(child, doc, indent + 1)
 
 def get_indent(indent):
     return "\t" * indent
 
 # GUI erstellen
 root = Tk()
-root.title("JSON Converter by Hasan")
+root.title("XML to Word Converter by Hasan")
 
 # Größe der GUI anpassen
 root.geometry("400x200")
@@ -56,10 +44,10 @@ root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
 root.grid_rowconfigure(1, weight=1)
 
-label = Label(root, text="Wähle eine JSON-Datei aus:")
+label = Label(root, text="Wähle eine XML-Datei aus:")
 label.pack(pady=20)
 
-button = Button(root, text="Datei auswählen", command=select_json_file)
+button = Button(root, text="Datei auswählen", command=select_xml_file)
 button.pack(pady=10)
 
 close_button = Button(root, text="Schließen", command=root.destroy)
